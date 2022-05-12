0b0e2ad975b9468572bcc634e46e1c5d7157f6ef
Andy <warmtree1699@gmail.com>
2022-05-13 01:17:30 +0800

    Update 2022-05-12T17:17:25.551Z

---
+++ assets/js/all.js
@@ -0,0 +1,12 @@
+"use strict";
+
+$(function () {
+  console.log('Hello Bootstrap5');
+});
+var swiper = new Swiper(".mySwiper", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  }
+});
+//# sourceMappingURL=all.js.map
