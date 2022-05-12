c5a31035cf65f0878a2a34fedd2b2ddc339153e9
Andy <warmtree1699@gmail.com>
2022-05-13 01:06:04 +0800

    first commit

---
+++ app/assets/js/all.js
@@ -0,0 +1,10 @@
+$(function() {
+  console.log('Hello Bootstrap5');
+});
+
+let swiper = new Swiper(".mySwiper", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
